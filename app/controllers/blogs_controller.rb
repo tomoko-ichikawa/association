c91ef9ed828d1d98ef5cd41398bc986f95131836
tomoko ichikawa <takator@yahoo.co.jp>
2019-06-11 11:01:25 +0900

    init

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,43 @@
+class BlogsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+    
+  def new
+      @blog = Blog.new
+  end
+  
+  def create
+      @blog = Blog.new(blog_params)
+      @blog.user_id = current_user.id
+      @blog.save
+      redirect_to blogs_path
+  end
+
+  def index
+      @blogs = Blog.all
+  end
+
+  def show
+      @blog = Blog.find(params[:id])
+      @user = @blog.user
+  end
+  
+  def edit
+      @blog = Blog.find(params[:id])
+      @user = @blog.user
+  end
+  
+  def update
+      @blog = Blog.find(params[:id])
+      @user = @blog.user
+      if @blog.update(blog_params)
+          redirect_to user_path,notice:"ブログを編集しました！"
+      else
+          render'edit'
+      end
+  end
+  
+  private
+    def blog_params
+        params.require(:blog).permit(:title, :content)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+    root 'blogs#index'
+    devise_for :users
+    resources :blogs
+    resources :users
+end
